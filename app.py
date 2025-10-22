8572988b22dbb11571160f4d4b9930eb0c7619e2
Kevin David Peña Avila <83792538+kevin50-html@users.noreply.github.com>
2025-10-21 21:47:21 -0500

    Cambios en App

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-# run.py
 from app import create_app, db, bcrypt
 from app.models.user import User
-from flask import Flask
-from app.controllers.main_controller import main_controller  # Importa tu blueprint
-app = Flask(__name__)
-# Registrar el blueprint
-app.register_blueprint(main_controller, url_prefix='/auth')  # Asegúrate de que tenga el prefijo correcto
 
+# Crear la instancia de la aplicación Flask usando la función create_app
 app = create_app()
 
+# Crear un usuario de prueba si no existe (esto solo se ejecuta una vez)
 with app.app_context():
     email = 'demo@demo.com'
     password = 'Demo1234'
 
     # Verificar si el usuario ya existe
     existing_user = User.query.filter_by(email=email).first()
-    
+
     if existing_user:
         print(f"El usuario con el correo {email} ya existe.")
     else:
@@ -31,5 +27,6 @@ with app.app_context():
 
         print(f"Usuario {email} creado exitosamente.")
 
+# Este bloque solo se ejecuta cuando corres la aplicación de forma local
 if __name__ == "__main__":
     app.run(debug=True)
