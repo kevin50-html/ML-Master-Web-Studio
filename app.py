41cb6d443db152f365513d56244303dabc0c2543
Kevin David Peña Avila <83792538+kevin50-html@users.noreply.github.com>
2025-10-22 00:08:07 -0500

    Cambios en App Prueba

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from app import create_app
 
+# Crear la instancia de la aplicación Flask usando la función create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Usa el servidor de desarrollo de Flask para pruebas locales
+    app.run(debug=True, host='0.0.0.0', port=5000)
